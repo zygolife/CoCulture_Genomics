c7331d7c627ad87e7ebbe8c361fd6fff274167b9
Jason Stajich <jason.stajich@ucr.edu>
2018-07-12 16:40:43 -0700

    Update get_unmapped_bbmap.sh

--- Map_and_Separate/jobs/get_unmapped_bbmap.sh
+++ Map_and_Separate/jobs/get_unmapped_bbmap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes 1 --ntasks 1 --mem 8G --time 5:00:00  -p shared
+
+#  if the BAM file is not created already, create it
 if [ ! -f CHZCO.bbmap.bam ]; then
  samtools view -b CHZCO.bbmap.sam > CHZCO.bbmap.bam
 fi
 
+# if the unmapped read file is not created, make it
 if [ ! -f unmapped_bbmap.both.bam ]; then
+# flag -f 12 means unmapped read
+# basically we process a BAM file, and output only the reads which did not align to the host genome
+# in order to extract a parasite genome reads
  samtools view -b -f 12 CHZCO.bbmap.bam > unmapped_bbmap.both.bam
 fi
